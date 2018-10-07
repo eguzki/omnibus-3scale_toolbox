e701fd4691ac32711b085d9c75c8bf661cba93e9
Eguzki Astiz Lezaun <eguzki.astiz@gmail.com>
2018-10-07 20:42:56 +0200

    fix entrypoint

--- config/projects/3scale_toolbox.rb
+++ config/projects/3scale_toolbox.rb
@@ -4,25 +4,29 @@
 # All Rights Reserved.
 #
 
-name "3scale_toolbox"
-maintainer "Eguzki Astiz Lezaun"
-homepage "https://github.com/3scale/3scale_toolbox"
+name '3scale_toolbox'
+maintainer 'Eguzki Astiz Lezaun'
+homepage 'https://github.com/3scale/3scale_toolbox'
 
 # Defaults to C:/3scale_toolbox on Windows
 # and /opt/3scale_toolbox on all other platforms
 install_dir "#{default_root}/#{name}"
 
-build_version "0.4.0"
+build_version '0.4.0'
+
 build_iteration 1
 
 # Creates required build directories
-dependency "preparation"
+dependency 'preparation'
 
-dependency "3scale_toolbox"
+override :ruby, version: '2.3.1'
+override :rubygems, version: '2.4.8'
+
+dependency '3scale_toolbox'
 
 # Version manifest file
-dependency "version-manifest"
+dependency 'version-manifest'
 
-exclude "**/.git"
-exclude "**/bundler/git"
-exclude "**/vendor"
+exclude '**/.git'
+exclude '**/bundler/git'
+exclude '**/vendor'
--- config/software/3scale_toolbox.rb
+++ config/software/3scale_toolbox.rb
@@ -1,9 +1,10 @@
 # These options are required for all software definitions
-name "3scale_toolbox"
-default_version "0.4.0"
+name '3scale_toolbox'
+default_version '0.4.0'
 
-dependency "ruby"
-dependency "rubygems"
+dependency 'ruby'
+dependency 'rubygems'
+dependency 'bundler'
 
 # Sources may be URLs, git locations, or path locations
 # Currently testing with specific patched release. Uncomment when not needed.
@@ -14,11 +15,23 @@ dependency "rubygems"
 #end
 
 # only to use patched release
-source url: "https://github.com/3scale/3scale_toolbox/archive/0.4.0_patched_ssl.tar.gz",
-       md5: "22eafe298a98c772481d0cda5d73b5a4"
-relative_path "3scale_toolbox-0.4.0_patched_ssl"
+source url: 'https://github.com/3scale/3scale_toolbox/archive/0.4.0_patched_ssl.tar.gz',
+       md5: '22eafe298a98c772481d0cda5d73b5a4'
+
+relative_path '3scale_toolbox-0.4.0_patched_ssl'
+
 build do
-  # vendor the gems required by the app
-  bundle "install --path vendor/bundle"
-  bundle "binstubs 3scale_toolbox --path #{install_dir}/bin"
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  # This installs dependencies, and creates a Gemfile.lock for use by
+  # appbundler later
+  bundle 'install --without development test --retry 3', env: env
+
+  # Now, we still need to actually install 3scale_toolbox in the embedded package
+  gem 'build 3scale_toolbox.gemspec', env: env
+  gem 'install ./3scale_toolbox-*.gem --local --no-ri --no-rdoc', env: env
+
+  # Now, create an 3scale binstub
+  gem 'install appbundler --version 0.10.0 --no-ri --no-rdoc', env: env
+  command "appbundler . '#{install_dir}/bin' 3scale_toolbox", env: env
 end
