f0a1b329ac63f6398aac8892d796b6025150e1b9
Eguzki Astiz Lezaun <eguzki.astiz@gmail.com>
2018-10-26 12:19:56 +0200

    nothing

--- config/software/3scale_toolbox.rb
+++ config/software/3scale_toolbox.rb
@@ -8,7 +8,7 @@ dependency 'bundler'
 
 version '0.5.0' do
   source url: "https://github.com/3scale/3scale_toolbox/archive/v#{version}.tar.gz",
-  md5: '95475ea169e618636e1d387c3ae43a87'
+    md5: '95475ea169e618636e1d387c3ae43a87'
 end
 
 relative_path "3scale_toolbox-#{version}"
